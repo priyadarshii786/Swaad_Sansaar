37f83c24b19b3e17c4b1bfc317e647fce51b4cb8
priyadarshii786 <tathagatp918@gmail.com>
2024-10-14 06:02:09 +0530

    C-05; Changed one line to see the recipe screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:swaad_sansaar/pages/home.dart';
+import 'package:swaad_sansaar/pages/recipe.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,15 +19,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: Recipe(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
 
   final String title;
 
